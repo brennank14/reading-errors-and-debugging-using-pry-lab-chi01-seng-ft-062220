7dfd2bd19ae9860614ab7b8ba8a5580ead3d8a87
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 19:45:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -2,18 +2,15 @@
 require 'pry'
 
 def generate_star_date
-  star_date = (rand(100000) + 400000) / 10.0
+  (rand(100000) + 400000) / 10.0
 end
 
 
 def state_log(star_date)
-  "Captain's Log, #{star_date} "
+  "Captain's Log, star date #{star_date} "
 end
 
 def engage
   date = generate_star_date
-  state_log(date)
+  puts state_log(date)
 end
-
-
-puts engage
